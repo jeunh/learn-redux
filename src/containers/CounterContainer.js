21d5129a9eca7ee4a86075957546b65097595f70
jeunh <gkwldms1218@gmail.com>
2023-07-03 22:52:31 +0900

    6-8. useSelector 최적화

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -6,10 +6,14 @@ import Counter from "../components/Counter";
 function CounterContainer() {
   // useSelector는 리덕스 스토어의 상태를 조회하는 Hook
   // state 값은 store.getState() 함수를 호출했을 때 나타나는 결과물과 동일
-  const {number, diff} = useSelector(state => ({
-    number: state.counter.number,
-    diff: state.counter.diff
-  }));
+  // const {number, diff} = useSelector(state => ({
+  //   number: state.counter.number,
+  //   diff: state.counter.diff
+  // }));
+
+  // useSelector 최적화
+  const number = useSelector(state => state.counter.number );
+  const diff = useSelector(state => state.counter.diff );
 
   // useDispatch는 리덕스 스토어의 dispatch를 함수에서 사용할 수 있게 해주는 Hook
   const dispatch = useDispatch();
